dfa7a6c63f4d820f515886bda9fc40d707fc5bca
wmin0 <wmin0@hotmail.com>
2012-05-03 21:09:12 +0800

    wmin0 : first installation with fb api key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectype::Application.config.secret_token = 'efde1fa97f3e17da65c50518d2f292771631c8db264db90545425f97ef034a98f1601a7966e8b5f5461d359390eb98a6b699b25820708f2ec60ee06d2cde926b'
