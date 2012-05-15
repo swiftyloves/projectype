beadd019f86f20e4906d287c640044db44533fb7
japie <jaywiewu@gmail.com>
2012-05-15 19:28:56 +0800

    add fake link and unlogin.js(haven't test)

--- app/assets/javascripts/index_unlogin.js
+++ app/assets/javascripts/index_unlogin.js
@@ -1,4 +1,25 @@
 
+(function(){
+	console.log('in the function')
 
-.fbicon{}
-$("#fbButton").trigger("click")
+
+}());
+
+
+$('.fbicon').click(function(){
+	console.log('fb.click')
+	$('#fbButton').trigger("click");
+});
+
+$('.g+icon').click(function(){
+	console.log('g+.click')
+	$('#g+Button').trigger("click");
+});
+
+$('.helpButton').click(function(){
+	console.log('helpButton.click')
+	$('#helpButton').trigger("click");
+});
+
+// .fbicon{}
+// $("#fbButton").trigger("click")
