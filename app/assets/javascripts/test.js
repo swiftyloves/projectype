a8d3ee69421e63d926dda5741dd3df1592b412ef
japie <jaywiewu@gmail.com>
2012-05-15 22:23:45 +0800

    japie: remove editable

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -13,24 +13,24 @@ $(document).ready(function() {
 			// right:cd  'month,agendaWeek,agendaDay'
 			right:''
 		},
-		// selectable: true,
-		// selectHelper: true,
-		// select: function(start, end, allDay) {
-		// 	var title = prompt('Event Title:');
-		// 	if (title) {
-				// calendar.fullCalendar('renderEvent',
-				// 	{
-				// 		title: title,
-				// 		start: start,
-				// 		end: end,
-				// 		allDay: allDay
-				// 	},
-				// 	true // make the event "stick"
-				// );
-		// 	}
-		// 	calendar.fullCalendar('unselect');
-		// },
-		editable: true,
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end, allDay) {
+			var title = prompt('Event Title:');
+			if (title) {
+				calendar.fullCalendar('renderEvent',
+					{
+						title: title,
+						start: start,
+						end: end,
+						allDay: allDay
+					},
+					true // make the event "stick"
+				);
+			}
+			calendar.fullCalendar('unselect');
+		},
+		// editable: true,
 		events: [
 			{
 				title: 'All Day Event',
