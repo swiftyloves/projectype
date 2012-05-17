6990a84d7ccb48bdca0d83cc81ca654c1f1d5a46
wmin0 <wmin0@hotmail.com>
2012-05-17 12:23:12 +0800

    wmin0: add project has no owner, auto delete

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,13 @@
 class Project < ActiveRecord::Base
   attr_accessible :name
   has_many :participates, :dependent => :destroy
-  has_many :users, :through => :participates
+  has_many :users, :through => :participates, :after_remove => :check_has_users
 
   has_many :tasks, :dependent => :destroy
+
+  def check_has_users (user)
+    if self.users.empty?
+      self.destroy
+    end
+  end
 end
