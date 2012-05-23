f7e3b4ffd7528f83c46ff44c642288591efc79bb
heatherchen <heatherchen@heatherchen.(none)>
2012-05-23 21:42:24 +0800

    project information from task

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -8,14 +8,28 @@ class TaskController < ApplicationController
   end
 
   def query
-      project = Project.find(params[:id]) 
-      puts project
-      proj_users = project.users
-      proj_tasks = project.tasks
-      puts proj_users[0].id
-      puts proj_tasks.length
-      render json: project #proj_users proj_tasks
-
+      @project = Project.find(params[:id]) 
+      puts @project
+      @proj_users = @project.users
+      @proj_tasks = @project.tasks
+      puts @proj_users[0].id
+      puts @proj_tasks.length
+      tmp = {}      
+      sub = []
+      @proj_tasks.each do |t|                                 
+         #task.append(t)  
+         if(t.subtasks.length>0)   
+            t.subtasks.each do |s|  
+              sub.append(s)
+            end  
+         end           
+      end 
+      tmp[:p] = @project
+      tmp[:u] = @proj_users
+      tmp[:t] = @proj_tasks   
+      tmp[:s] = sub    
+      render json: tmp
+      #render json: project #proj_users proj_tasks    
   end
 
 
