6fecf83640392744b2ad478ed788431d4be76295
wmin0 <wmin0@hotmail.com>
2012-05-31 00:54:40 +0800

    wmin0: participate msg & help#login

--- app/assets/javascripts/help.js
+++ app/assets/javascripts/help.js
@@ -1,8 +1,10 @@
 $(function(){
-	console.log('lafNA; Gi:UG')
-
 	jQuery('#helpage').accordion({ 
     	autoHeight: false
     	// clearStyle:true
 	});
+        $("#login").click(function(event) {
+          $("#helpContent").dialog("close");
+          $("#logButton").trigger("click");
+        }); 
 });
